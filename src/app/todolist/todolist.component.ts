00ef5cc7e9489e051408fb745c03dd535ef29b5a
Luan Henique Campos Soares <lhcampossoares@gmail.com>
2025-02-02 19:18:06 -0300

    :sparkles: feat: Saving in localhost

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -7,7 +7,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./todolist.component.css'],
 })
 export class TodolistComponent implements OnInit {
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getFromLocalStorage();
+  }
 
   categories = ['Personal', 'Work', 'Studies', 'Others'];
   taskArray = [
@@ -27,24 +29,47 @@ export class TodolistComponent implements OnInit {
         isEditable: false,
         category: form.controls['category'].value || 'Others',
       });
+
+      this.saveToLocalStorage();
+
       form.reset();
+    }
+  }
+
+  saveToLocalStorage() {
+    let stringJSON =  JSON.stringify(this.taskArray);
+    localStorage.setItem('todolist', stringJSON);
+  }
+
+  getFromLocalStorage(){
+    let itemsJSONString = localStorage.getItem('todolist')
+    if(itemsJSONString != null) {
+      this.taskArray = JSON.parse(itemsJSONString);
     }
   }
 
   onDelete(index: number) {
     this.taskArray.splice(index, 1);
+
+    this.saveToLocalStorage();
+  }
+
+  onCheck(index: number) {
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+
+    this.saveToLocalStorage();
   }
 
   onEdit(index: number) {
     this.taskArray[index].isEditable = true;
+
+    this.saveToLocalStorage();
   }
 
   onSave(index: number, newtask: string) {
     this.taskArray[index].taskName = newtask;
     this.taskArray[index].isEditable = false;
-  }
 
-  onCheck(index: number) {
-    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+    this.saveToLocalStorage();
   }
 }
